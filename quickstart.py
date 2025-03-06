1fb2d2bc53579e9f433a6864fd9d3533d15bf4b8
gangok <kstylee@gmail.com>
2025-03-07 02:04:11 +0900

    Fix bug on num_trains_to_ignore

--- quickstart.py
+++ quickstart.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
         num_passenger = str(config['num_passenger'])
 
         num_trains_to_check = int(config['num_trains_to_check'])
+        num_trains_to_ignore = int(config['num_trains_to_ignore'])
         want_reserve = bool(config['want_reserve'])
         notify_sound_file_path = config['notify_sound_file_path']
 
@@ -39,8 +40,9 @@ if __name__ == "__main__":
         dpt_tm = cli_args.tm
 
         num_trains_to_check = cli_args.num
+        num_trains_to_ignore = 0
         want_reserve = cli_args.reserve
         notify_sound_file_path = os.path.join(os.path.dirname(__file__), 'mp3', '예약이준비되었습니다_papago.mp3')
 
-    srt = SRT(dpt_stn, arr_stn, dpt_dt, dpt_tm, num_trains_to_check, want_reserve, notify_sound_file_path, telegram_client, num_passenger)
+    srt = SRT(dpt_stn, arr_stn, dpt_dt, dpt_tm, num_trains_to_check, num_trains_to_ignore, want_reserve, notify_sound_file_path, telegram_client, num_passenger)
     srt.run(login_id, login_psw)
